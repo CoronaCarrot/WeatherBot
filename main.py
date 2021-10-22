1ef89d1a90e8a7e7b65deef701d61475cd83e9a6
CarrotWillKillU <Carr0twillkillu@outlook.com>
2021-10-22 21:54:45 +0100

    1.0

--- main.py
+++ main.py
@@ -9,16 +9,16 @@
 """
 Module Imports
 """
-import discord
-from discord.ext import commands
 from termcolor import cprint, colored
 import os
-import time
-import json
-import random
 from discordpy_slash.slash import *
+from random import randint
 
-
+firstboot = 0
+"""
+Checking to make sure the correct config files exist
+if they do not they are created with default settings
+"""
 if os.path.exists(os.getcwd() + "/config.json"):
 
     with open("./config.json") as f:
@@ -31,6 +31,20 @@ else:
 
     with open(os.getcwd() + "/config.json", "w+") as f:
         json.dump(configTemplate, f)
+
+    print(colored("―――――――――――――――", "blue"))                            # |
+    print(colored("》", "blue"), "    Weather Bot    ", colored("《", "blue"))     # | Sends Bot branding to console
+    print(colored("   •", "blue"), "version  0.0.1", colored("•", "blue"))        # | and sends version info
+    print(colored("―――――――――――――――", "blue"))                            # |
+    print()
+    cprint('⚙️Beginning First Boot Setup', 'blue')  # Sends in console that the bot is beginning first time setup
+    time.sleep(randint(0, 3))  # Pause For Effect
+    cprint('⚙️Creating Config Files', 'blue')  # Sends in console that the bot is creating config files
+    time.sleep(randint(0, 3))  # Pause For Effect
+    with open("./config.json") as f:
+        configData = json.load(f)
+    firstboot = 1
+
 
 if os.path.exists(os.getcwd() + "/icons.json"):
     with open("./icons.json") as f:
@@ -86,29 +100,69 @@ intents = discord.Intents.default()
 
 intents.members = True
 
-bot = commands.Bot(command_prefix=configData["Prefix"], intents=intents)
+bot = commands.Bot(command_prefix=[configData["Prefix"], "/"], intents=intents)
 
+if configData["Token"] == "YOUR BOT TOKEN" or configData["Prefix"] == "PREFIX HERE" or configData["Owner"] == "OWNER ID HERE" or configData["WeatherAPIKey"] == "API KEY HERE":
+    if firstboot == 1:
+        pass
+    else:
+        print(colored("―――――――――――――――", "blue"))                            # |
+        print(colored("》", "blue"), "    Weather Bot    ", colored("《", "blue"))     # | Sends Bot branding to console
+        print(colored("   •", "blue"), "version  0.0.1", colored("•", "blue"))        # | and sends version info
+        print(colored("―――――――――――――――", "blue"))                            # |
+    print()
+    print(colored("――――――――――――――――", "blue"))
+    print(colored("》", "blue"), "   Error Handling   ", colored("《", "blue"))
+    print(colored("    •", "blue"), f'version 0.0.1', colored("•", "blue"))
+    print()
+    print(colored("Error Code » ", "blue"), f'ConfigError')
+    print(colored("Error Message » ", "blue"), f'Detected Unedited Config. Please Setup Config Before Running This Bot')
+    print(colored("Reference » ", "blue"), f'config.json')
+    print(colored("――――――――――――――――", "blue"))
+    exit(0)
+else:
+    pass
 
+"""
+All events ran on startup
+"""
 @bot.event
 async def on_ready():
-    print(colored("―――――――――――――――", "blue"))
-    print(colored("》", "blue"), "    Weather Bot    ", colored("《", "blue"))
-    print(colored("   •", "blue"), "version  0.0.1", colored("•", "blue"))
-    print(colored("―――――――――――――――", "blue"))
+    if firstboot == 1:
+        pass
+    else:
+        print(colored("―――――――――――――――", "blue"))                            # |
+        print(colored("》", "blue"), "    Weather Bot    ", colored("《", "blue"))     # | Sends Bot branding to console
+        print(colored("   •", "blue"), "version  0.0.1", colored("•", "blue"))        # | and sends version info
+        print(colored("―――――――――――――――", "blue"))                            # |
     print()
-    cprint('⚙️Syncing Commands', 'blue')
-    prm = configData["Presence"]["Message"]
-    if configData["Presence"]["Type"].lower == "watching":
+    cprint('⚙️Accepting Commands', 'blue')  # Sends in console that the bot is now excepting discord commands
+    """
+    Sets the bots status in discord depending on the information given in the config
+    """
+    prm = configData["Presence"]["Message"]  # Gets Status Message From Config
+    if str(configData["Presence"]["Type"]).lower() == "watching":
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=prm))
-    elif configData["Presence"]["Type"].lower == "playing":
+        cprint(f'⚙️Set Status To "Watching {prm}"', 'blue')
+    elif str(configData["Presence"]["Type"]).lower() == "playing":
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=prm))
-    elif configData["Presence"]["Type"].lower == "listening":
+        cprint(f'⚙️Set Status To "Playing {prm}"', 'blue')
+    elif str(configData["Presence"]["Type"]).lower() == "listening":
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=prm))
+        cprint(f'⚙️Set Status To "Listening to {prm}"', 'blue')
     else:
         pass
+    """
+    Syncs bots commands on startup
+    """
+    cprint('⚙️Syncing Commands', 'blue')
     await sync_all_commands(bot)
 
-
+"""
+Resyncs all commands for the bot.
+this command is only executable by the bot developer (me)
+and is used for debugging purposes only
+"""
 @bot.command()
 async def resync(ctx):
     if ctx.message.author.id == 642729210368098324:
@@ -124,24 +178,33 @@ async def resync(ctx):
 
 @bot.command()
 async def weather(ctx, location):
-
     print(colored(f'⚠️Executing Command "Weather" For {str(ctx.message.author)}', "blue"))
-    locationr = location.replace(" ", "%20")
-    apikey = configData["WeatherAPIKey"]
+    locationr = location.replace(" ", "%20")  # Replace Spaces With The URL Equivalent (So API Request Link Valid)
+    apikey = configData["WeatherAPIKey"]  # Pull API Key From Config
     locationsearch = requests.get(
-        f'https://dataservice.accuweather.com/locations/v1/search?apikey={apikey}&q={locationr}&language=en-us&details=true')
-    responsestr = locationsearch.text
-    responsejson = locationsearch.json()
-
-    iserror = True
+        f'https://dataservice.accuweather.com/locations/v1/search?apikey={apikey}&q={locationr}&language=en-us'
+        f'&details=true')  # Searches Weather API For Location Key Of Searched Town
+    responsestr = locationsearch.text  # Returns Text Output From API
+    responsejson = locationsearch.json()  # Returns JSON Output From API
 
     if responsestr[0] == '[':
+        """
+        Checks To Make Sure The API Has Found A Town/City
+        if the API fails to find a country it will return blank
+        """
         if len(responsejson) >= 1:
-            responsejson = responsejson[0]
+            responsejson = responsejson[0]  # Gets JSON From Search
 
             iserror = False
 
             if iserror:
+                """
+                Sends an error handling message if there is an error with the API call
+                Added mainly to catch the error sent when the APIs limit of 50 calls per day has been reached
+                
+                This section also sends a discord embed to the player informing them of the error,
+                telling them (Or The Developer) to check console and providing an error code
+                """
                 print(colored("――――――――――――――――", "blue"))
                 print(colored("》", "blue"), "   Error Handling   ", colored("《", "blue"))
                 print(colored("    •", "blue"), f'version 0.0.1', colored("•", "blue"))
@@ -151,15 +214,25 @@ async def weather(ctx, location):
                 print(colored("Reference » ", "blue"), f'{responsejson["Reference"]}')
                 print(colored("――――――――――――――――", "blue"))
 
-                embed = discord.Embed(title="⚠️ | An Error Occurred", description="An error occurred while executing this command.\nCheck console for more info.", color=0xea4d65)
+                embed = discord.Embed(title="⚠️ | An Error Occurred", description="An error occurred while executing "
+                                                                                  "this command.\nCheck console for "
+                                                                                  "more info.", color=0xea4d65)
                 embed.add_field(name=f'Error Code', value=f' `{responsejson["Code"]}`', inline=True)
                 await ctx.send(embed=embed)
             else:
-                locationresponse = locationsearch.json()[0]
-                resultkey = locationresponse["Key"]
+                """
+                The section of code between "# START OF INFO GATHERING #" and "# END OF INFO GATHERING #"
+                takes information out of the code and puts them into separate variables to be used for discord embeds
+                It also has logic that checks information to tell the bot what emojis to use next to certain info
+                EG - The thermometer emoji will show as cold if the temperature is below 5°C or 41°F
+                """
+                # START OF INFO GATHERING #
+                locationresponse = locationsearch.json()[0]  # Pull JSON Data From Returned List (API Returns List)
+                resultkey = locationresponse["Key"]  # Location Key For Finding Weather Data
                 weatherdata = requests.get(
-                    f'https://dataservice.accuweather.com/currentconditions/v1/{resultkey}?apikey={apikey}&language=en-us&details=true')
-                wdr = weatherdata.json()[0]
+                    f'https://dataservice.accuweather.com/currentconditions/v1/{resultkey}?apikey={apikey}&language'
+                    f'=en-us&details=true')  # Request Weather Data For Found Town/City
+                wdr = weatherdata.json()[0]  # Pull JSON Data From Returned List (API Returns List)
                 tempcr = wdr["RealFeelTemperature"]["Metric"]["Value"]  # RealFeel Metric
                 tempfr = wdr["RealFeelTemperature"]["Imperial"]["Value"]  # RealFeel Imperial
                 tempc = wdr["Temperature"]["Metric"]["Value"]  # Temperature Metric
@@ -169,37 +242,42 @@ async def weather(ctx, location):
                 wsd = wdr["Wind"]["Direction"]["English"]  # Wind Speed Direction
                 gsi = wdr["WindGust"]["Speed"]["Imperial"]["Value"]  # Wind Gusts Imperial
                 gsm = wdr["WindGust"]["Speed"]["Imperial"]["Value"]  # Wind Gusts Metric
-                lube = wdr["LocalObservationDateTime"].split("T")
-                lube = lube[1].split("+")
-                lube = lube[0].split(":")
-                if int(lube[0]) > 12:
-                    lube[0] = int(lube[0]) - 12
-                    lube.append("PM")
-                else:
-                    lube.append("AM")
+                lube = wdr["LocalObservationDateTime"].split("T")   # |
+                lube = lube[1].split("+")                           # | Gets Last Update Time In Readable Form
+                lube = lube[0].split(":")                           # |
+                if int(lube[0]) > 12:               # |
+                    lube[0] = int(lube[0]) - 12     # |
+                    lube.append("PM")               # | Converts The 24 Hour Time To 12 Hour And Appends AM or PM
+                else:                               # |
+                    lube.append("AM")               # |
 
-                if tempc <= 5 or tempf <= 41:
-                    hoc = icondata["31"]
-                else:
-                    hoc = icondata["30"]
-                if tempcr <= 5 or tempfr <= 41:
-                    hocr = icondata["31"]
-                else:
-                    hocr = icondata["30"]
+                if tempc <= 5 or tempf <= 41:       # |
+                    hoc = icondata["31"]            # |
+                else:                               # |
+                    hoc = icondata["30"]            # | Checks To See If The Temperature is below 5°C or 41°F
+                if tempcr <= 5 or tempfr <= 41:     # | Then Sets The Thermometer Emoji To Either Hot Or Cold
+                    hocr = icondata["31"]           # |
+                else:                               # |
+                    hocr = icondata["30"]           # |
 
-                if wdr["WeatherIcon"] < 10:
-                    pfx = f'0{wdr["WeatherIcon"]}'
-                else:
-                    pfx = f'{str(wdr["WeatherIcon"])}'
+                if wdr["WeatherIcon"] < 10:             # |
+                    pfx = f'0{wdr["WeatherIcon"]}'      # | Pre Formatting For Next Section
+                else:                                   # | Adds A Zero Before Single Digit Emoji IDs
+                    pfx = f'{str(wdr["WeatherIcon"])}'  # |
 
-                icl = icondata[str(wdr["WeatherIcon"])]
-                icl = icl.removeprefix(f'<:{pfx}:')
-                icl = icl.replace(">", "")
-                icl = f'https://cdn.discordapp.com/emojis/{icl}.png?size=2048'
+                icl = icondata[str(wdr["WeatherIcon"])]                         # |
+                icl = icl.removeprefix(f'<:{pfx}:')                             # | Converts A Discord Emoji To A PNG
+                icl = icl.replace(">", "")                                      # | For The Embed ThumbNail
+                icl = f'https://cdn.discordapp.com/emojis/{icl}.png?size=2048'  # |
+                # END OF INFO GATHERING #
 
+                """
+                Pieces all the Metric information into an easy to read discord embed
+                with a prompt telling the user to react to the embed (message) to receive Imperial Readings
+                """
                 embed = discord.Embed(
-                    title=f'{icondata[str(wdr["WeatherIcon"])]} It Is Currently {wdr["WeatherText"]} In {locationresponse["LocalizedName"]}',
-                    color=0x53b9e4)
+                    title=f'{icondata[str(wdr["WeatherIcon"])]} It Is Currently {wdr["WeatherText"]} In '
+                          f'{locationresponse["LocalizedName"]}', color=0x53b9e4)
                 embed.set_thumbnail(url=f'{icl}')
                 embed.add_field(name=f'Temperature', value=f'{hoc} `{tempc}°C`', inline=True)
                 embed.add_field(name=f'Feels Like', value=f'{hocr} `{tempcr}°C`', inline=True)
@@ -208,9 +286,14 @@ async def weather(ctx, location):
                 embed.add_field(name="Wind Speeds", value=f'{icondata["32"]} `{wsd} {wsm} km/h`', inline=True)
                 embed.add_field(name="Gusts", value=f'{icondata["32"]} `{gsm} km/h`', inline=True)
                 embed.add_field(name=f'_ _', value=f'_ _', inline=True)
-                embed.set_footer(text=f'React To Get Imperial Readings | Last Updated {lube[0]}:{lube[1]} {lube[3]}')
+                embed.set_footer(text=f'React To Get Imperial Readings | Last Updated {lube[0]}:{lube[1]} {lube[3]} '
+                                      f'(GMT)')
                 await ctx.send(embed=embed)
 
+                """
+                checking for the reaction being added to the embed message by the player
+                with a timeout of 30 second.
+                """
                 def check(reaction, user):
                     return user == ctx.message.author and reaction.message.author.bot
 
@@ -219,8 +302,14 @@ async def weather(ctx, location):
                 except asyncio.TimeoutError:
                     pass
                 else:
+                    """
+                    Pieces all the Imperial information into an easy to read discord embed
+                    And either edits the Embed (slash command based) or sends a new embed (Prefix Based)
+                    to show that new information
+                    """
                     edit = discord.Embed(
-                        title=f'{icondata[str(wdr["WeatherIcon"])]} Imperial Readings For {locationresponse["LocalizedName"]}',
+                        title=f'{icondata[str(wdr["WeatherIcon"])]} Imperial Readings For '
+                              f'{locationresponse["LocalizedName"]}',
                         color=0x53b9e4)
                     edit.set_thumbnail(url=f'{icl}')
                     edit.add_field(name=f'Temperature', value=f'{hoc} `{tempf}°F`', inline=True)
@@ -231,17 +320,13 @@ async def weather(ctx, location):
                     edit.add_field(name=f'_ _', value=f'_ _', inline=True)
                     await ctx.send(embed=edit)
         else:
-            print(colored("――――――――――――――――", "blue"))
-            print(colored("》", "blue"), "   Error Handling   ", colored("《", "blue"))
-            print(colored("    •", "blue"), f'version 0.0.1', colored("•", "blue"))
-            print()
-            print(colored("Error Code » ", "blue"), f'Index Error')
-            print(colored("Error Message » ", "blue"), f'Index Out Of Range')
-            print(colored("Reference » ", "blue"), f'line 127')
-            print(colored("――――――――――――――――", "blue"))
-
-            ior = discord.Embed(title="⚠️ | We Searched The World", description="Couldn't find this Town/City\nApi returned blank\nmake sure you are searching for a town/city and not a country!", color=0xea4d65)
-            ior.add_field(name=f'Error Code', value=f' `UnknownLocation`', inline=True)
+            """
+            This is executed if the API does not find a Town/City
+            it sends a discord embed indicating to the player that their search was a fail
+            and advises them to make sure they have searched for a Town/City and not a country
+            """
+            ior = discord.Embed(title=f"We Couldn't Find This Town/City!",
+                                description="Try Searching For Town/City And Not Country", color=0xea4d65)
             await ctx.send(embed=ior)
 
 
