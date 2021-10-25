d6e1bfe7b91f935521631a144c77b94f96bc150f
CarrotWillKillU <Carr0twillkillu@outlook.com>
2021-10-25 23:39:42 +0100

    1.1.2 (Fixed)

--- Cogs/DeleteData.py
+++ Cogs/DeleteData.py
@@ -4,7 +4,8 @@ import uuid
 
 import discord
 from discord.ext.commands import Bot, Cog
-from discord_slash import cog_ext, SlashContext
+from discord_slash import cog_ext, SlashContext, ButtonStyle, ComponentContext
+from discord_slash.utils.manage_components import create_button, create_actionrow, wait_for_component
 
 from main import configData, bot
 
@@ -17,26 +18,26 @@ class DeleteData(Cog):
     async def _deletedata(self, ctx: SlashContext, user=None):
         if ctx.author.id == 642729210368098324:
             if type(user) == str and str(user).lower() == "all":
+                buttons = [create_button(style=ButtonStyle.blurple, label="Yes", emoji="✅", custom_id="Yes"), create_button(style=ButtonStyle.blurple, label="No", emoji="❌", custom_id="No")]
+                action_row = create_actionrow(*buttons)
+
                 embed = discord.Embed(title="⚙️ | Are You Sure?",
                                       description=f'This will delete **ALL** user\ndata registered to this bot.\nDo you wish'
-                                                  f' to continue?\n`y` or `n`', color=0xf63737)
-                edit = await ctx.send(embed=embed)
-                print(edit)
+                                                  f' to continue?', color=0xf63737)
+                edit = await ctx.send(embed=embed, components=[action_row], hidden=True)
 
-                def check(msg):
-                    return msg.author == ctx.author and msg.channel == ctx.channel and \
-                           msg.content.lower() in ["y", "n"]
+                def check(ctx: ComponentContext):
+                    return ctx.author == ctx.author and ctx.channel == ctx.channel and ctx.component_id in ["Yes", "No"]
 
                 try:
-                    msg = await bot.wait_for("message", check=check, timeout=10)  # 10 seconds to reply
-                    await msg.delete()
-                    if msg.content.lower() == "y":
+                    button_ctx: ComponentContext = await wait_for_component(bot, components=action_row, timeout=15, check=check)  # 10 seconds to reply
+                    if button_ctx.component_id == "Yes":
                         if configData["Require Auth Code For High Security Commands"]:
                             try:
-                                embed = discord.Embed(title="⚙️ | Detected High Security Command Usage",
-                                                      description=f'Please enter Auth Code\nsent to console',
+                                embed = discord.Embed(title="Detected High Security Command Usage",
+                                                      description=f'Please enter Auth Code sent to console',
                                                       color=0xf63737)
-                                await edit.edit(embed=embed)
+                                await ctx.send(embed=embed, components=[], hidden=True)
                                 authcode = uuid.uuid4()
                                 print(f'Your auth code is: {authcode}')
 
@@ -53,19 +54,19 @@ class DeleteData(Cog):
                                     for file in filtered_files:
                                         path_to_file = os.path.join(directory, file)
                                         os.remove(path_to_file)
-                                    embed = discord.Embed(title="⚙️ | All User Data Deleted",
+                                    embed = discord.Embed(title="All User Data Deleted",
                                                           description=f'deleted data for All Users', color=0xf63737)
-                                    await edit.edit(embed=embed)
+                                    await ctx.send(embed=embed, hidden=True)
                                 else:
-                                    embed = discord.Embed(title="⚙️ | Command Canceled",
+                                    embed = discord.Embed(title="Command Canceled",
                                                           description=f'You provided an invalid authcode',
                                                           color=0xf63737)
-                                    await edit.send(embed=embed)
+                                    await ctx.send(embed=embed, hidden=True)
 
                             except asyncio.TimeoutError:
-                                embed = discord.Embed(title="⚙️ | Deletion canceled",
+                                embed = discord.Embed(title="Deletion canceled",
                                                       description=f'You didn''t reply in time!', color=0xf63737)
-                                await edit.edit(embed=embed)
+                                await ctx.send(embed=embed, components=[], hidden=True)
                         else:
                             directory = "./UserData"
                             files_in_directory = os.listdir(directory)
@@ -73,22 +74,23 @@ class DeleteData(Cog):
                             for file in filtered_files:
                                 path_to_file = os.path.join(directory, file)
                                 os.remove(path_to_file)
-                            embed = discord.Embed(title="⚙️ | All User Data Deleted",
+                            embed = discord.Embed(title="All User Data Deleted",
                                                   description=f'deleted data for All Users', color=0xf63737)
-                            await ctx.send(embed=embed)
+                            await ctx.send(embed=embed, components=[], hidden=True)
 
-                    elif msg.content.lower() == "n":
-                        embed = discord.Embed(title="⚙️ | Deletion canceled",
+                    elif button_ctx.component_id == "No":
+                        embed = discord.Embed(title="Deletion canceled",
                                               description=f'canceled deletion for all users', color=0xf63737)
-                        await ctx.send(embed=embed)
+                        await ctx.send(embed=embed, components=[], hidden=True)
                     else:
-                        embed = discord.Embed(title="⚙️ | Deletion canceled",
+                        embed = discord.Embed(title="Deletion canceled",
                                               description=f'You provided an invalid response', color=0xf63737)
-                        await ctx.send(embed=embed)
+                        await ctx.send(embed=embed, components=[], hidden=True)
                 except asyncio.TimeoutError:
-                    embed = discord.Embed(title="⚙️ | Deletion canceled",
+
+                    embed = discord.Embed(title="Deletion canceled",
                                           description=f'You didn''t reply in time!', color=0xf63737)
-                    await ctx.send(embed=embed)
+                    await ctx.send(embed=embed, components=[], hidden=True)
             elif user.startswith("<@"):
                 user = user.replace("<", "")
                 user = user.replace(">", "")
@@ -97,23 +99,23 @@ class DeleteData(Cog):
                 file = "./UserData/{0}.json".format(str(user))
                 try:
                     os.remove(file)
-                    embed = discord.Embed(title="⚙️ | Users Data Deleted",
+                    embed = discord.Embed(title="Users Data Deleted",
                                           description=f'deleted data for <@{user}>', color=0xf63737)
-                    await ctx.send(embed=embed)
+                    await ctx.send(embed=embed, components=[], hidden=True)
                 except FileNotFoundError:
-                    embed = discord.Embed(title="⚠️ | User Does Not Exist",
+                    embed = discord.Embed(title="User Does Not Exist",
                                           description=f'User ID "{user}" does not have any data stored.',
                                           color=0xf63737)
-                    await ctx.send(embed=embed)
+                    await ctx.send(embed=embed, components=[], hidden=True)
             else:
                 embed = discord.Embed(title="<:Error:764493646199521291> | Incorrect Syntax",
                                       description="`/deletedata <user/all>`", color=0xf63737)
-                await ctx.send(embed=embed)
+                await ctx.send(embed=embed, components=[], hidden=True)
 
         else:
             embed = discord.Embed(title="<:Error:764493646199521291> | Permission Error",
                                   description="Only bot developers can execute this command", color=0xf63737)
-            await ctx.send(embed=embed)
+            await ctx.send(embed=embed, components=[], hidden=True)
 
 
 def setup(bot: Bot):
--- Cogs/help.py
+++ Cogs/help.py
@@ -1,6 +1,12 @@
+import asyncio
+
 import discord
 from discord.ext.commands import Bot, Cog
-from discord_slash import cog_ext, SlashContext
+from discord_slash import cog_ext, SlashContext, ComponentContext
+from discord_slash.utils.manage_components import create_select, create_select_option, create_actionrow, \
+    wait_for_component
+
+from main import bot
 
 
 class Help(Cog):
@@ -9,6 +15,17 @@ class Help(Cog):
 
     @cog_ext.cog_slash(name="help")
     async def _help(self, ctx: SlashContext):
+        select = create_select(
+            options=[  # the options in your dropdown
+                create_select_option("User Commands", value="ucmds", emoji="👤"),
+                create_select_option("Dev Commands", value="dcmds", emoji="💻"),
+            ],
+            placeholder="Filter = None",  # the placeholder text to show when no options have been chosen
+            min_values=1,  # the minimum number of options a user must select
+            max_values=2,  # the maximum number of options a user can select
+        )
+        action_row = create_actionrow(select)
+
         help = discord.Embed(title="Bot Commands",
                              description="All commands have the / prefix and have auto-completion", color=0x53b9e4)
         help.add_field(name=f'/Weather `Location`', value=f'Fetches The Current Weather For A Location, Based On Your '
@@ -17,7 +34,105 @@ class Help(Cog):
         help.add_field(name=f'/debug **[DEV]**', value=f'Fetches UpStats for the bot. Used for debugging the host and '
                                                        f'bad ping.', inline=False)
         help.add_field(name=f'/antisteal **[DEV]**', value=f'This Is A Secret :smirk:', inline=False)
-        await ctx.send(embed=help)
+        edit = await ctx.send(embed=help, components=[action_row])
+
+        loop = True
+        while loop:
+            try:
+                select_ctx: ComponentContext = await wait_for_component(bot, components=[action_row], timeout=30)
+                #await select_ctx.defer(ignore=True)
+                print(select_ctx.selected_options)
+                if select_ctx.selected_options == ['ucmds']:
+
+                    select = create_select(
+                        options=[  # the options in your dropdown
+                            create_select_option("User Commands", value="ucmds", emoji="👤"),
+                            create_select_option("Dev Commands", value="dcmds", emoji="💻"),
+                        ],
+                        placeholder="Filter = User Commands",
+                        # the placeholder text to show when no options have been chosen
+                        min_values=1,  # the minimum number of options a user must select
+                        max_values=2,  # the maximum number of options a user can select
+                    )
+                    action_row = create_actionrow(select)
+
+                    filtu = discord.Embed(title="Bot Commands",
+                                          description="All commands have the / prefix and have auto-completion",
+                                          color=0x53b9e4)
+                    filtu.add_field(name=f'/Weather `Location`',
+                                    value=f'Fetches The Current Weather For A Location, Based On Your '
+                                          f'Search.', inline=False)
+                    filtu.add_field(name=f'/Help', value=f'Sends This Message', inline=False)
+                    await edit.edit(embed=filtu, components=[action_row])
+                    #await select_ctx.edit_origin(content="Cumu")
+                elif select_ctx.selected_options == ['dcmds']:
+                    #select_ctx.responded = True
+
+                    select = create_select(
+                        options=[  # the options in your dropdown
+                            create_select_option("User Commands", value="ucmds", emoji="👤"),
+                            create_select_option("Dev Commands", value="dcmds", emoji="💻"),
+                        ],
+                        placeholder="Filter = Dev Commands",
+                        # the placeholder text to show when no options have been chosen
+                        min_values=1,  # the minimum number of options a user must select
+                        max_values=2,  # the maximum number of options a user can select
+                    )
+                    action_row = create_actionrow(select)
+
+                    filtd = discord.Embed(title="Bot Commands",
+                                          description="All commands have the / prefix and have auto-completion",
+                                          color=0x53b9e4)
+                    filtd.add_field(name=f'/debug **[DEV]**',
+                                    value=f'Fetches UpStats for the bot. Used for debugging the host and '
+                                          f'bad ping.', inline=False)
+                    filtd.add_field(name=f'/antisteal **[DEV]**', value=f'This Is A Secret :smirk:', inline=False)
+                    await edit.edit(embed=filtd, components=[action_row])
+                    #await select_ctx.edit_origin(content="Cumd")
+                elif select_ctx.selected_options == ['ucmds', 'dcmds'] or select_ctx.selected_options == ['dcmds',
+                                                                                                          'ucmds']:
+                    #select_ctx.responded = True
+
+                    select = create_select(
+                        options=[  # the options in your dropdown
+                            create_select_option("User Commands", value="ucmds", emoji="👤"),
+                            create_select_option("Dev Commands", value="dcmds", emoji="💻"),
+                        ],
+                        placeholder="Filter = None",  # the placeholder text to show when no options have been chosen
+                        min_values=1,  # the minimum number of options a user must select
+                        max_values=2,  # the maximum number of options a user can select
+                    )
+                    action_row = create_actionrow(select)
+
+                    filtn = discord.Embed(title="Bot Commands",
+                                          description="All commands have the / prefix and have auto-completion",
+                                          color=0x53b9e4)
+                    filtn.add_field(name=f'/Weather `Location`',
+                                    value=f'Fetches The Current Weather For A Location, Based On Your '
+                                          f'Search.', inline=False)
+                    filtn.add_field(name=f'/Help', value=f'Sends This Message', inline=False)
+                    filtn.add_field(name=f'/debug **[DEV]**',
+                                    value=f'Fetches UpStats for the bot. Used for debugging the host and '
+                                          f'bad ping.', inline=False)
+                    filtn.add_field(name=f'/antisteal **[DEV]**', value=f'This Is A Secret :smirk:', inline=False)
+                    await edit.edit(embed=filtn, components=[action_row])
+                    #await select_ctx.edit_origin(content="Cuma")
+                await select_ctx.defer(ignore=True)
+            except asyncio.TimeoutError:
+                select = create_select(
+                    options=[  # the options in your dropdown
+                        create_select_option("ucmds", value="User Commands", emoji="👤"),
+                        create_select_option("dcmds", value="Dev Commands", emoji="💻"),
+                    ],
+                    placeholder="Filter = None",  # the placeholder text to show when no options have been chosen
+                    min_values=1,  # the minimum number of options a user must select
+                    max_values=2,  # the maximum number of options a user can select
+                    disabled=True,
+                )
+                action_row = create_actionrow(select)
+
+                await edit.edit(components=[action_row])
+                loop = False
 
 
 def setup(bot: Bot):
